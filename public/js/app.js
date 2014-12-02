4563902245a32effe86a938c10c286da3f6b5b42
kronus <tam@kronusproductions.com>
2014-12-02 06:52:31 -0600

    wanted to make sure all services were in the songService.js file. Just clean up

--- public/js/app.js
+++ public/js/app.js
@@ -8,31 +8,3 @@ var songApp = angular.module('songApp', ['mainCtrl', 'songService', 'SharedServi
     .run(function($http,CSRF_TOKEN){
         $http.defaults.headers.common['csrf_token'] = CSRF_TOKEN;
     });
-
-angular.module('SharedServices', [])
-    .config(function ($httpProvider) {
-        $httpProvider.responseInterceptors.push('myHttpInterceptor');
-        var spinnerFunction = function (data, headersGetter) {
-            // todo start the spinner here
-            $('#loading').show();
-            return data;
-        };
-        $httpProvider.defaults.transformRequest.push(spinnerFunction);
-    })
-// register the interceptor as a service, intercepts ALL angular ajax http calls
-    .factory('myHttpInterceptor', function ($q, $window) {
-        return function (promise) {
-            return promise.then(function (response) {
-                // do something on success
-                // todo hide the spinner
-                $('#loading').hide();
-                return response;
-
-            }, function (response) {
-                // do something on error
-                // todo hide the spinner
-                $('#loading').hide();
-                return $q.reject(response);
-            });
-        };
-    });
